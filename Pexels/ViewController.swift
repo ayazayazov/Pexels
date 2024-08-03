e985b803e3cbcea25e1bae6affe8eaf8d1ea763b
ayazayazov <ayazayazov00@gmail.com>
2024-08-04 00:12:52 +0400

    project files uploaded

---
+++ Pexels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pexels
+//
+//  Created by Ayaz Ayazov on 04.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
