46ecd75b0cf69d67fb7c8f47109ca5378ee0b070
ayazayazov <ayazayazov00@gmail.com>
2024-09-04 15:00:33 +0400

    photoDetail network manager & endpoint fixed

--- Pexels/Network/Photos/PhotosEndpoint.swift
+++ Pexels/Network/Photos/PhotosEndpoint.swift
@@ -9,12 +9,16 @@ import Foundation
 
 enum PhotosEndpoint {
     case curatedPhotos(page: Int?, perPage: Int?)
+    case photoID(id: Int?)
     
     var path: String {
         switch self {
         case .curatedPhotos(let page, let perPage):
             return "curated?page=\(page ?? 1)&per_page=\(perPage ?? 1)"
+        case .photoID(let id):
+            return "photos/\(id ?? 123453)"
         }
+
         
     }
 }
--- Pexels/Network/Photos/PhotosManager.swift
+++ Pexels/Network/Photos/PhotosManager.swift
@@ -9,9 +9,15 @@ import Foundation
 
 protocol PhotosManagerUseCase {
     func getCuratedPhotos(page: Int?, perPage: Int?, completion: @escaping((Photo?, String?) -> Void))
+    func getPhotoID(id: Int?, completion: @escaping((PhotoData?, String?) -> Void))
 }
 
 class PhotosManager: PhotosManagerUseCase {
+    func getPhotoID(id: Int?, completion: @escaping ((PhotoData?, String?) -> Void)) {
+        let endpoint = PhotosEndpoint.photoID(id: id)
+        NetworkManager.request(model: PhotoData.self, endpoint: endpoint.path, completion: completion)
+    }
+    
     func getCuratedPhotos(page: Int?, perPage: Int?, completion: @escaping ((Photo?, String?) -> Void)) {
         let endpoint = PhotosEndpoint.curatedPhotos(page: page, perPage: perPage)
         NetworkManager.request(model: Photo.self, endpoint: endpoint.path, completion: completion)
