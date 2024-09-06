e66722b63d568d67b80c3669e0fb25afb0a0d135
ayazayazov <ayazayazov00@gmail.com>
2024-09-06 23:54:42 +0400

    favoritesVC ui update

--- Pexels/Scene/Favorites/FavoritesVC.swift
+++ Pexels/Scene/Favorites/FavoritesVC.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
+
 class FavoritesVC: UIViewController {
+    
+    private let feedLabel: UILabel = {
+        let il = UILabel()
+        il.text = "Saved Posts"
+        il.font = .systemFont(ofSize: 30, weight: .bold)
+        il.textAlignment = .left
+        il.translatesAutoresizingMaskIntoConstraints = false
+        return il
+    }()
+    
+    private let feed: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 30
+        let cv = UICollectionView(frame: .init(), collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        viewSetup()
+        configureViewModel()
     }
- 
+    
+    func configureViewModel() {
+        }
 
+    private func viewSetup() {
+        NSLayoutConstraint.activate([
+        ])
+    }
 }
