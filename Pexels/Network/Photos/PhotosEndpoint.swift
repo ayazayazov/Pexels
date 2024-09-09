2c412be250540cba2a7b8ad1c8a3a93c4d11f527
ayazayazov <ayazayazov00@gmail.com>
2024-09-09 23:10:41 +0400

    search for photos network manager configuration

--- Pexels/Network/Photos/PhotosEndpoint.swift
+++ Pexels/Network/Photos/PhotosEndpoint.swift
@@ -10,6 +10,7 @@ import Foundation
 enum PhotosEndpoint {
     case curatedPhotos(page: Int?, perPage: Int?)
     case photoID(id: Int?)
+    case searchForPhotos(query: String?, page: Int?, perPage: Int?)
     
     var path: String {
         switch self {
@@ -17,6 +18,8 @@ enum PhotosEndpoint {
             return "v1/curated?page=\(page ?? 1)&per_page=\(perPage ?? 1)"
         case .photoID(let id):
             return "v1/photos/\(id ?? 123453)"
+        case .searchForPhotos(let query, let page, let perPage):
+            return "v1/search?query=\(query ?? "")&page=\(page ?? 1)&per_page=\(perPage ?? 1)"
         }
     }
 }
--- Pexels/Network/Photos/PhotosManager.swift
+++ Pexels/Network/Photos/PhotosManager.swift
@@ -10,9 +10,15 @@ import Foundation
 protocol PhotosManagerUseCase {
     func getCuratedPhotos(page: Int?, perPage: Int?, completion: @escaping((Photo?, String?) -> Void))
     func getPhotoID(id: Int?, completion: @escaping((PhotoData?, String?) -> Void))
+    func getSearchForPhotos(query: String?, page: Int?, perPage: Int?, completion: @escaping((Photo?, String?) -> Void))
 }
 
 class PhotosManager: PhotosManagerUseCase {
+    func getSearchForPhotos(query: String?, page: Int?, perPage: Int?, completion: @escaping ((Photo?, String?) -> Void)) {
+        let endpoint = PhotosEndpoint.searchForPhotos(query: query, page: page, perPage: perPage)
+        NetworkManager.request(model: Photo.self, endpoint: endpoint.path, completion: completion)
+    }
+    
     func getPhotoID(id: Int?, completion: @escaping ((PhotoData?, String?) -> Void)) {
         let endpoint = PhotosEndpoint.photoID(id: id)
         NetworkManager.request(model: PhotoData.self, endpoint: endpoint.path, completion: completion)
