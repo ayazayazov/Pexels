65cac5dce5630b9f4b054891211a331cd1e60baf
ayazayazov <ayazayazov00@gmail.com>
2024-09-09 23:11:21 +0400

    seach for videos network manager configuration

--- Pexels/Network/Videos/VideosEndpoint.swift
+++ Pexels/Network/Videos/VideosEndpoint.swift
@@ -9,11 +9,14 @@ import Foundation
 
 enum VideosEndpoint {
     case popularVideos(page: Int?, perPage: Int?)
+    case searchForVideos(query: String?, page: Int?, perPage: Int?)
     
     var path: String {
         switch self {
         case .popularVideos(page: let page, perPage: let perPage):
             return "videos/popular?page=\(page ?? 1)&per_page=\(perPage ?? 2)"
+        case .searchForVideos(query: let query, page: let page, perPage: let perPage):
+            return "videos/search?query=\(query ?? "nature")&page=\(page ?? 1)&per_page=\(perPage ?? 1)"
         }
     }
 }
--- Pexels/Network/Videos/VideosManager.swift
+++ Pexels/Network/Videos/VideosManager.swift
@@ -9,9 +9,15 @@ import Foundation
 
 protocol VideosManagerUseCase {
     func getPopularVideos(page: Int?, perPage: Int?, completion: @escaping((Video?, String?) -> Void))
+    func getsearchForVideos(query: String, page: Int?, perPage: Int?, completion: @escaping((Video?, String?) -> Void))
 }
 
 class VideosManager: VideosManagerUseCase {
+    func getsearchForVideos(query: String, page: Int?, perPage: Int?, completion: @escaping ((Video?, String?) -> Void)) {
+        let endpoint = VideosEndpoint.searchForVideos(query: query, page: page, perPage: perPage)
+        NetworkManager.request(model: Video.self, endpoint: endpoint.path, completion: completion)
+    }
+    
     func getPopularVideos(page: Int?, perPage: Int?, completion: @escaping ((Video?, String?) -> Void)) {
         let endpoint = VideosEndpoint.popularVideos(page: page, perPage: perPage)
         NetworkManager.request(model: Video.self, endpoint: endpoint.path, completion: completion)
